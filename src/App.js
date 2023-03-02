58a286dc7a73a6180c7db5a0e3d14b3d10c92a0e
sumon <sumonmondal1530765@gmail.com>
2023-03-03 03:13:17 +0600

    design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Setting from "./pages/Setting";
+import Sidebar from "./components/Sidebar";
 
+
+export const ExpenseContext = createContext()
 function App() {
+  const [allExpenses, setAllExpenses] = useState([
+    {
+      id: 1,
+      title: "Grocery",
+      amount: 100,
+      category: "food",
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 2,
+      title: "Shopping",
+      amount: 200,
+      category: "shopping",
+      date: new Date(2021, 2, 28),
+    },
+
+  ])
+  const [totalBalance, setTotalBalance] = useState(2300)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ExpenseContext.Provider value={{allExpenses,setAllExpenses,totalBalance,setTotalBalance}}>
+      <BrowserRouter>
+        <div className="dashboard-content">
+          <Sidebar />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/ratings" element={<Dashboard />} />
+            <Route path="/bar" element={<Dashboard />} />
+            <Route path="/reports" element={<Dashboard />} />
+            <Route path="/settings" element={<Setting />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ExpenseContext.Provider>
   );
 }
 
